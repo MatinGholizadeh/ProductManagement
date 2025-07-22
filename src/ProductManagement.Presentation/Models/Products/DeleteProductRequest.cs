9fd795789b9dcc4f47961d1381484be781a5922b
Matin gholizadeh <matin.gh.work@gmail.com>
2025-07-23 01:38:31 +0330

    bugs fixed, needed some Dto

---
+++ src/ProductManagement.Presentation/Models/Products/DeleteProductRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace ProdManagement.Presentation.Models.Products;
+
+public class DeleteProductRequest
+{
+    public string ProductName { get; set; } = default!;
+}
---
+++ src/ProductManagement.Presentation/Models/Products/UpdateProductRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ProdManagement.Presentation.Models.Products;
+
+public class UpdateProductRequest
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public bool? IsAvailable { get; set; }
+}
